40c3ff40d134c0b48693e677d78eacbcae0e8527
snowlizard <illegal_burrito@live.com>
2025-07-26 09:46:43 -0700

    add draggable window

--- client/src/components/Window.tsx
+++ client/src/components/Window.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+
 interface windowProps {
     title: string,
     path: string,
@@ -5,10 +7,15 @@ interface windowProps {
 }
 
 export const Window = (props: windowProps) => {
+    const windowID = props.contextID;
+
+    useEffect(() => {
+        dragWindow(document.getElementById(windowID)!);
+    }, [windowID]);
 
     return (
         <div className="background">
-            <div className="window glass active" style={{width: "1000px", height: "600px"}}>
+            <div id={windowID} className="window glass active" style={{width: "1000px", height: "600px"}}>
                 <div className="title-bar">
                     <div className="title-bar-text">{props.title}</div>
                     <div className="title-bar-controls">
@@ -24,4 +31,39 @@ export const Window = (props: windowProps) => {
             </div>
         </div>
     );
+}
+
+const dragWindow = (element: HTMLElement) => {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    const dragMouseDown = (e: MouseEvent) => {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;    
+    }
+
+    const elementDrag = (e: MouseEvent) => {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    const closeDragElement = () => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+    if(document.getElementById(element.id)){
+        document.getElementById(element.id)!.onmousedown = dragMouseDown;
+    } else {
+        element.onmousedown = dragMouseDown;
+    }
 }
