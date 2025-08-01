bbbc0691ced46742990b3be742bc32b238f0aae2
snowlizard <illegal_burrito@live.com>
2025-08-01 10:18:26 -0700

    refactoring remove unneeded comment

--- server/desktop.js
+++ server/desktop.js
@@ -4,18 +4,6 @@ const getDesktopApps = async () => {
     const filePath = "../client/public/C:/Users/Snowlizard/Desktop/apps.json";
     let data = await fs.readFile(filePath, "utf-8");
     return JSON.parse(data);
-    
-    /**
-    const DESKTOP = "../client/public/C:/Users/Snowlizard/Desktop";
-
-    let temp = await fs.readdir(DESKTOP);
-    
-    for(const file of temp){
-        let filePath = DESKTOP + "/" + file;
-        files.push(JSON.parse(await fs.readFile(filePath, "utf-8")))
-    }
-    return files;
-    **/
 }
 
 module.exports = {
