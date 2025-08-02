16bc41bb5500ef3c8c1ee4615a13f090ffb77266
snowlizard <illegal_burrito@live.com>
2025-08-02 13:10:59 -0700

    taskbar app tiles added

--- client/src/components/taskbar.tsx
+++ client/src/components/taskbar.tsx
@@ -1,14 +1,30 @@
 import { useSelector } from "react-redux";
+import { type app } from "../types/app";
 
 export const Taskbar = () => {
     const activeApps = useSelector((state: any) => state.app.running);
+
+    const AppTile = (props: app) => (
+        <div className="appTile">
+            <div className="tileIcon">
+                <img src={props.icon} />
+            </div>
+            <span className="tileTitle">{props.title}</span>
+        </div>
+    );
 
     return (
         <>
             <div id="start_orb"></div>            
             <div id="taskbar">
-                <div style={{background: "green", width: "100px"}}>
-                    <span>Pokedex</span>
+                <div className="tileContainer">
+                    {
+                        activeApps == null ? "" :
+                        activeApps.map( (app: app) => <AppTile {...app} />)
+                    }
+                </div>
+                <div className="systemtray">
+
                 </div>
             </div>
         </>
