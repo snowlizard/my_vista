b40563a956f0524e8b76c30ebe0dbb4343e12ff0
snowlizard <illegal_burrito@live.com>
2025-08-09 14:05:42 -0700

    adding react draggable

--- client/src/components/Window.tsx
+++ client/src/components/Window.tsx
@@ -1,8 +1,11 @@
+import Draggable from 'react-draggable';
+import { useRef } from 'react';
 import { useDispatch } from "react-redux";
 import { deactiveApp, resizeMin, resizeMax } from "../contexts/appSlice";
 import { type app } from "../types/app";
 
 export const Window = (props: app) => {
+    const nodeRef = useRef(null);
     const dispatch = useDispatch<any>();
 
     const handleClose = (event: any) => {
@@ -19,21 +22,23 @@ export const Window = (props: app) => {
     }
 
     return (
-        <div className="background vista-window" id={props.index}>
-            <div className="window glass active">
-                <div className="title-bar">
-                    <div className="title-bar-text">{props.title}</div>
-                    <div className="title-bar-controls">
-                        <button onClick={minimize} aria-label="Minimize"></button>
-                        <button onClick={maximize} aria-label="Maximize"></button>
-                        <button onClick={handleClose} aria-label="Close"></button>
+        <Draggable nodeRef={nodeRef}>
+            <div className="background vista-window" id={props.index} ref={nodeRef}>
+                <div className="window glass active">
+                    <div className="title-bar">
+                        <div className="title-bar-text">{props.title}</div>
+                        <div className="title-bar-controls">
+                            <button onClick={minimize} aria-label="Minimize"></button>
+                            <button onClick={maximize} aria-label="Maximize"></button>
+                            <button onClick={handleClose} aria-label="Close"></button>
+                        </div>
                     </div>
-                </div>
 
-            <div className="window-body has-space" style={{height: "95%"}}>
-                    <embed style={{width: "100%", height: "100%"}} src={props.entry} />
-                </div> 
+                <div className="window-body has-space" style={{height: "95%"}}>
+                        <embed style={{width: "100%", height: "100%"}} src={props.entry} />
+                    </div> 
+                </div>
             </div>
-        </div>
+        </Draggable>
     );
 }
