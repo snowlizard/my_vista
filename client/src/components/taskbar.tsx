e40df27bbf5fecec80de4bec15fa771148f5ed71
snowlizard <illegal_burrito@live.com>
2025-08-13 20:10:46 -0700

    added taskbar clock

--- client/src/components/taskbar.tsx
+++ client/src/components/taskbar.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { menuState } from "../contexts/startmenuSlice";
 import { resizeMin } from "../contexts/appSlice";
@@ -6,27 +7,6 @@ import { type app } from "../types/app";
 export const Taskbar = () => {
     const activeApps = useSelector((state: any) => state.app.running);
     const dispatch = useDispatch<any>();
-
-    const AppTile = (props: app) => {
-
-        const highestZ = activeApps.reduce((accumulator: any, appTwo: app) => Math.max(accumulator, appTwo.zIndex), 0);
-        const appZ = activeApps.filter((targetApp: app) => targetApp.zIndex === highestZ);
-        
-        let tileClasses = "appTile";
-        if(appZ[0].index === props.index && !props.hidden){
-            tileClasses += " appActive"
-        }
-
-        return (
-            <div className={tileClasses} id={`appTile-${props.index}`}
-                onClick={() => dispatch(resizeMin(props))}>
-                <div className="tileIcon">
-                    <img src={props.icon} />
-                </div>
-                <span className="tileTitle">{props.title}</span>
-            </div>
-        );
-    }
 
     return (
         <div id="taskbar">
@@ -38,9 +18,45 @@ export const Taskbar = () => {
                     activeApps.map( (app: app) => <AppTile key={app.index} {...app} />)
                 }
             </div>
-            <div className="systemtray">
+            <Systemtray />
+        </div>
+    );
+}
 
+const AppTile = (props: app) => {
+    const activeApps = useSelector((state: any) => state.app.running);
+    const dispatch = useDispatch<any>();
+    const highestZ = activeApps.reduce((accumulator: any, appTwo: app) => Math.max(accumulator, appTwo.zIndex), 0);
+    const appZ = activeApps.filter((targetApp: app) => targetApp.zIndex === highestZ);
+    
+    let tileClasses = "appTile";
+    if(appZ[0].index === props.index && !props.hidden){
+        tileClasses += " appActive"
+    }
+
+    return (
+        <div className={tileClasses} id={`appTile-${props.index}`}
+            onClick={() => dispatch(resizeMin(props))}>
+            <div className="tileIcon">
+                <img src={props.icon} />
             </div>
+            <span className="tileTitle">{props.title}</span>
+        </div>
+    );
+}
+
+const Systemtray = () => {
+    const [time, setTime] = useState(new Date().toLocaleTimeString().replace(/\:\d{2}\s/, " "));
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date().toLocaleTimeString().replace(/\:\d{2}\s/, " "));
+        }, 50000);
+    }, []);
+
+    return (
+        <div className="systemtray">
+            <div className="systemclock">{time}</div>
         </div>
     );
 }
