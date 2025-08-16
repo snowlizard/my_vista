177e36b5819066c4046bb4b18a3349f4d5db6191
snowlizard <illegal_burrito@live.com>
2025-08-15 20:42:09 -0700

    removed login not used

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bootReducer from './contexts/bootSlice';
-import loginReducer from './contexts/loginSlice';
 import appReducer from './contexts/appSlice';
 import themeReducer from './contexts/themeSlice';
 import startmenuReducer from './contexts/startmenuSlice';
@@ -8,7 +7,6 @@ import startmenuReducer from './contexts/startmenuSlice';
 export const store = configureStore({
   reducer: {
     boot: bootReducer,
-    login: loginReducer,
     app: appReducer,
     theme: themeReducer,
     startmenu: startmenuReducer
